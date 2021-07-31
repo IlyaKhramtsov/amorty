777e2c046c443b7af0fa6b84c4f5a83e453e4639
Ilya <ilya.kramt@gmail.com>
2021-07-31 18:02:11 +0300

    add check date

--- loan_calculator/utils.py
+++ loan_calculator/utils.py
@@ -1,8 +1,15 @@
 import datetime
+import holidays
 
 
 def convert_date(date):
     """Convert string to datetime.date object"""
     if isinstance(date, str):
         return datetime.date.fromisoformat(date)
+    return date
+
+def check_date(date: datetime.date) -> datetime.date:
+    """Checks and returns a date after a day off."""
+    while date.weekday() in holidays.WEEKEND or date in holidays.RUS():
+        date += datetime.timedelta(days=1)
     return date
