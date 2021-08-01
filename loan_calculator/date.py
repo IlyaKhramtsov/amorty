8a1219cce86b7edaea95f246a33d2948054d367d
Ilya <ilya.kramt@gmail.com>
2021-08-01 19:58:37 +0300

    add days counter method

--- loan_calculator/date.py
+++ loan_calculator/date.py
@@ -1,7 +1,7 @@
 import calendar
 import datetime
 
-from typing import Union
+from typing import Union, List
 
 from . import utils
 
@@ -23,7 +23,7 @@ class LoanDate:
         else:
             raise ValueError('Date must be string type or datetime.date type')
 
-    def get_dates(self):
+    def get_working_dates(self):
         """Creates a list of dates excluding weekends."""
         dates = []
         current_date = self._date
@@ -36,5 +36,26 @@ class LoanDate:
             period -= 1
         return dates
 
+    def get_count_days(self) -> List[Union[datetime.timedelta, list[datetime.timedelta]]]:
+        """Calculates the difference between dates in a list."""
+        days = []
+        dates = self.get_working_dates()
+        start_date = self._date
+        days.append(dates[0] - start_date)
+
+        for date, next_date in zip(dates, dates[1:]):
+            start_year = datetime.date(next_date.year, 1, 1)
+
+            if date.month == 12 and calendar.isleap(next_date.year):
+                common_date, leap_date = start_year - date, next_date - start_year
+                days.append([common_date, leap_date])
+
+            elif date.month == 12 and calendar.isleap(date.year):
+                leap_date, common_date = start_year - date, next_date - start_year
+                days.append([common_date, leap_date])
+
+            else:
+                days.append(next_date - date)
+        return days
 
 
