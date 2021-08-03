e2109594bfc0e4e5a182ae81a7f3e913e7d2e781
Ilya <ilya.kramt@gmail.com>
2021-08-03 23:45:54 +0300

    add abstract class Loan

---
+++ loan_calculator/loan.py
@@ -0,0 +1,58 @@
+import datetime
+from abc import ABC, abstractmethod
+from typing import Union
+
+from date import LoanDate
+
+class BaseLoan(ABC):
+    """Abstract class for building different kinds of loans"""
+
+    def __init__(self, amount: Union[int, float],
+                period: int,
+                rate: Union[int, float],
+                date: Union[datetime.date, str]):
+        self.amount = amount
+        self.period = period
+        self.rate = rate
+        self.date = LoanDate(period, date)
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount: Union[int, float]):
+        if isinstance(amount, (int, float)) and amount > 0:
+            self._amount = amount
+        else:
+            raise ValueError("Amount must be int or float type and positive number")
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, rate: Union[int, float]):
+        if isinstance(rate, (int, float)) and rate > 0:
+            self._rate = rate
+        else:
+            raise ValueError("Interest rate must be int or float type and positive number")
+
+    @proterty
+    def period(self):
+        return self._period
+
+    @period.setter
+    def period(self, period: int):
+        if isinstance(period, int) and period > 0:
+            self._period = period
+        else:
+            raise ValueError("Period must be integer type and positive number")
+
+    def __str__(self):
+        return f"\namount: {self._amount}\nrate: {self._rate}%\nperiod: {self._period} months"
+
+    @abstractmethod
+    def amortize(self):
+        pass
+
