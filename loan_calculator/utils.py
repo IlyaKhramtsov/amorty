4efffcb8f2fdd75dcc45758bf3fb9b2a8eeb14b4
Ilya <ilya.kramt@gmail.com>
2021-08-05 23:25:23 +0300

    add calculation of the type of year

--- loan_calculator/utils.py
+++ loan_calculator/utils.py
@@ -1,6 +1,12 @@
 import datetime
+import calendar
 import holidays
 
+
+DAYS_IN_YEAR = {
+        "common year": 365,
+        "leap year": 366,
+        }
 
 def convert_date(date):
     """Convert string to datetime.date object"""
@@ -13,3 +19,13 @@ def check_date(date: datetime.date) -> datetime.date:
     while date.weekday() in holidays.WEEKEND or date in holidays.RUS():
         date += datetime.timedelta(days=1)
     return date
+
+def set_days_count(day: datetime.timedelta, date: datetime.date) -> float:
+    if isinstance(day, list):
+        return (day[0].days / DAYS_IN_YEAR["common year"]) + (day[1].days / DAYS_IN_YEAR["leap year"])
+    return day.days / check_year_type(date)
+
+def check_year_type(date: datetime.date) -> int:
+    if calendar.isleap(date.year):
+        return DAYS_iN_YEAR["common year"]
+    return DAYS_IN_YEAR["leap year"]
