c13f6a28656c19801e84a848b5847bbcb0930d11
Ilya <ilya.kramt@gmail.com>
2021-08-18 22:27:33 +0300

    add type hints

--- loan_calculator/utils.py
+++ loan_calculator/utils.py
@@ -1,7 +1,7 @@
 import datetime
 import calendar
 import holidays
-from typing import Union, List
+from typing import Union, List, Iterator
 
 
 DAYS_IN_YEAR = {
@@ -9,7 +9,7 @@ DAYS_IN_YEAR = {
         "leap year": 366,
         }
 
-def convert_date(date):
+def convert_date(date: Union[str, datetime.date]) -> datetime.date:
     """Convert string to datetime.date object"""
     if isinstance(date, str):
         return datetime.date.fromisoformat(date)
@@ -28,11 +28,12 @@ def set_days_count(day: datetime.timedelta, date: datetime.date) -> float:
 
 def check_year_type(date: datetime.date) -> int:
     if calendar.isleap(date.year):
-        return DAYS_iN_YEAR["leap year"]
+        return DAYS_IN_YEAR["leap year"]
     return DAYS_IN_YEAR["common year"]
 
-def clear_days(days: List[Union[datetime.timedelta, List[datetime.timedelta]]]):
+def clear_days(days: List[Union[datetime.timedelta, List[datetime.timedelta]]]) -> Iterator[int]:
     for day in days:
         if isinstance(day, list):
             yield (day[0] + day[1]).days
-        yield day.days
+        else:
+            yield day.days
