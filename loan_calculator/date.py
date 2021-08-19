715375da9c2aa1003a32f9f84c6697f07069b310
Ilya <ilya.kramt@gmail.com>
2021-08-20 00:12:22 +0300

    add docstrings and fix lint errors

--- loan_calculator/date.py
+++ loan_calculator/date.py
@@ -1,14 +1,33 @@
+"""Module for loan.py, providing some additional facilities"""
+
 import calendar
 import datetime
 
-from typing import Union, List
+from typing import Union
 
 from loan_calculator import utils
 
 
 class LoanDate:
+    """LoanDate implements the dates of the loan payments.
+
+    Attributes
+    ----------
+    period: int, requared
+        Loan term (specified in mounths)
+    date : str, datetime.date, requared
+        Date of issue of the loan
+
+    Methods
+    -------
+    get_working_dates()
+        Calculates the dates on which the monthly payments on the loan will be paid
+    get_count_days()
+        Calculates the number of days between monthly loan payments
+    """
 
     def __init__(self, period: int, date: Union[str, datetime.date]) -> None:
+        """Initialize loan date"""
         self.period = period
         self.date = date
 
@@ -21,22 +40,24 @@ class LoanDate:
         if isinstance(date, (str, datetime.date)):
             self._date = utils.convert_date(date)
         else:
-            raise ValueError('Date must be string type or datetime.date type')
+            raise ValueError("Date must be string type or datetime.date type")
 
-    def get_working_dates(self):
+    def get_working_dates(self) -> list[datetime.date]:
         """Creates a list of dates excluding weekends."""
         dates = []
         current_date = self._date
         period = self.period
 
         while period:
-            days_in_current_month = calendar.monthrange(current_date.year, current_date.month)[1]
+            days_in_current_month = calendar.monthrange(
+                current_date.year, current_date.month
+            )[1]
             current_date += datetime.timedelta(days=days_in_current_month)
             dates.append(utils.check_date(current_date))
             period -= 1
         return dates
 
-    def get_count_days(self) -> List[Union[datetime.timedelta, list[datetime.timedelta]]]:
+    def get_count_days(self) -> list[datetime.timedelta]:
         """Calculates the difference between dates in a list."""
         days = []
         dates = self.get_working_dates()
@@ -57,5 +78,3 @@ class LoanDate:
             else:
                 days.append(next_date - date)
         return days
-
-
