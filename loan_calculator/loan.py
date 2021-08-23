97953a00daa0b775ff7e47ca0e0a901442f82344
Ilya <ilya.kramt@gmail.com>
2021-08-23 23:35:27 +0300

    add docstrings to methods

--- loan_calculator/loan.py
+++ loan_calculator/loan.py
@@ -1,21 +1,33 @@
 import datetime
 from abc import ABC, abstractmethod
 from collections import namedtuple
-from typing import Union, List
+from typing import Union, List, Iterator
 
 from loan_calculator.date import LoanDate
 from loan_calculator import utils
 
 
-LoanDetails = namedtuple('LoanDetails', 'date day principal interest payment balance')
+LoanDetails = namedtuple("LoanDetails", "date day principal interest payment balance")
+
 
 class Loan(ABC):
     """Abstract class for building different kinds of loans"""
 
-    def __init__(self, amount: Union[int, float],
-                period: int,
-                rate: Union[int, float],
-                date: Union[datetime.date, str]):
+    def __init__(
+        self,
+        amount: Union[int, float],
+        period: int,
+        rate: Union[int, float],
+        date: Union[datetime.date, str],
+    ):
+        """Construct a new loan.
+
+        Args:
+            amount: loan amount
+            period: loan term in months
+            rate: annual percentage rate
+            date: date of issue of the loan
+        """
         self.amount = amount
         self.period = period
         self.rate = rate
@@ -41,7 +53,9 @@ class Loan(ABC):
         if isinstance(rate, (int, float)) and rate > 0:
             self._rate = rate
         else:
-            raise ValueError("Interest rate must be int or float type and positive number")
+            raise ValueError(
+                "Interest rate must be int or float type and positive number"
+            )
 
     @property
     def period(self):
@@ -61,35 +75,55 @@ class Loan(ABC):
     def amortize(self):
         pass
 
-    def _calculate_accrued_interest(self, balance_reminder: int, day: datetime.timedelta, date: datetime.date) -> float:
+    def _calculate_accrued_interest(
+        self,
+        balance_reminder: Union[float, int],
+        day: Union[datetime.timedelta, List[datetime.timedelta]],
+        date: datetime.date,
+    ) -> float:
+        """Calculates accrued interest in the current period.
+
+        Args:
+            balance_reminder: balance in the current period
+            day: number of days in the current period
+            date: payment date
+        Returns:
+            float
+        """
         interest_rate = self._rate / 100
         days_to_year_ratio = utils.convert_days_to_year(day, date)
         return balance_reminder * interest_rate * days_to_year_ratio
 
-    def create_loan(self):
+    def create_loan(self) -> Iterator:
         loan_amortization = self.amortize()
         dates = self.date.get_working_dates()
         days = utils.clear_days(self.date.get_count_days())
 
-        for date, day, (payment, balance, principal, interest) in zip(dates, days, loan_amortization):
+        for date, day, (payment, balance, principal, interest) in zip(
+            dates, days, loan_amortization
+        ):
             loan_details = LoanDetails(date, day, principal, interest, payment, balance)
             yield loan_details
 
-    def show_summary(self):
-        pass
-
 
 class AnnuityLoan(Loan):
+    """Annuity loan type.
+
+    Implements the calculation of payments for the annuity scheme of repayment.
+    """
 
     def _calculate_monthly_interest(self) -> float:
         """Calculates monthly interest rate"""
         return self._rate / 1200
 
-    def get_annuity_payment(self) -> float:
+    def _get_annuity_coefficient(self) -> float:
         monthly_rate = self._calculate_monthly_interest()
         numerator = monthly_rate * (1 + monthly_rate) ** self._period
         denominator = (1 + monthly_rate) ** self._period - 1
-        return self._amount * (numerator / denominator)
+        return numerator / denominator
+
+    def get_annuity_payment(self):
+        return self._amount * self._get_annuity_coefficient()
 
     def _calculate_principal(self, accrued_interest: float) -> float:
         return self.get_annuity_payment() - accrued_interest
@@ -102,7 +136,9 @@ class AnnuityLoan(Loan):
         day = (x for x in self.date.get_count_days())
 
         while period:
-            accrued_interest = self._calculate_accrued_interest(balance_reminder, next(day), next(date))
+            accrued_interest = self._calculate_accrued_interest(
+                balance_reminder, next(day), next(date)
+            )
             principal = self._calculate_principal(accrued_interest)
             balance_reminder -= principal
             if period <= 1:
@@ -118,14 +154,13 @@ class DifferentiatedLoan(Loan):
 
     Implements the calculation of payments for the differentiated scheme of repayment.
     The differentiated payment method implies that:
-        - the principal amount is distributed over the period of payment in the equal 
+        - the principal amount is distributed over the period of payment in the equal
           installments;
         - the loan interest accrued on the balance.
 
     Methods
     --------
     amortize(): Calculates amortization for a differentiated repayment scheme
-           
     """
 
     def _calculate_principal(self) -> float:
@@ -139,7 +174,9 @@ class DifferentiatedLoan(Loan):
         day = (x for x in self.date.get_count_days())
 
         while period:
-            accrued_interest = self._calculate_accrued_interest(balance_reminder, next(day), next(date))
+            accrued_interest = self._calculate_accrued_interest(
+                balance_reminder, next(day), next(date)
+            )
             principal = self._calculate_principal()
             payment = principal + accrued_interest
             balance_reminder -= principal
