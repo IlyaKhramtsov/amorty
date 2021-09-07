bbaf3271e957ca0a4e89dd1e6a2e4f03f9bfbf88
Ilya <ilya.kramt@gmail.com>
2021-09-08 01:21:59 +0300

    update version: 0.1.0 -> 0.1.1

--- amorty/__init__.py
+++ amorty/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.1.0'
+# -*- coding: utf-8 -*-
+
+"""amorty"""
+
+from amorty.loan import Annuity, StraightLine
+from amorty.loan_format import TableFormat, ExcelFormat
+
+__all__ = ['Annuity', 'StraightLine', 'TableFormat', 'ExcelFormat']
+
+__version__ = '0.1.1'
